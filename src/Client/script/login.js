15d6ab8f03b700c1580f089dd5a810ad9a05293d
John Carlo M. Lorieta <116395590+Lorieta@users.noreply.github.com>
2025-01-13 10:29:38 +0600

    GOODS LOGIN, SIMPLE lang muna, sa susunod na hashing

---
+++ src/Client/script/login.js
@@ -0,0 +1,28 @@
+async function handleLogin(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('http://localhost:3000/src/Server/api/login.php', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password }),
+        });
+
+        const data = await response.json();
+
+        if (response.status === 200) {
+            alert('Login successful!');
+            window.location.href = '/src/Client/home.html';
+
+        } else {
+            alert(data.message);
+        }
+    } catch (error) {
+        alert('An error occurred. Please try again.');
+        console.error(error);
+    }
+}
+
+document.querySelector('form').addEventListener('submit', handleLogin);
